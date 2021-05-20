8889f6bc5c74e873b17f78083c7502e2ff124701
Halil Dogan <olush.dogan2gmail.com>
2021-05-20 18:32:46 -0400

    v1

---
+++ README.md
@@ -0,0 +1,38 @@
+# react-lapadi-subscribe
+
+React subscribe form for lapadi App.
+
+Working demo : https://lapadi.github.io/react-lapadi-subscribe/
+
+## Usage
+
+Create a list on lapadi, add a form and get its "action" attribute from the lapadi UI
+
+The `LapadiSubscribe` gives you a render prop with a `subscribe` method that you can call with your data.
+
+In your app :
+
+```jsx
+import LapadiSubscribe from "react-lapadi-subscribe"
+
+const url = "//xxxx.lapadi.com/subscribe/post?u={client_id}&id={form_id}";
+
+// simplest form (only email)
+const SimpleForm = () => <LapadiSubscribe url={url}/>
+
+// use the render prop and your custom form
+const CustomForm = () => (
+  <LapadiSubscribe
+    url={url}
+    render={({ subscribe, status, message }) => (
+      <div>
+        <CustomForm onSubmitted={formData => subscribe(formData)} />
+        {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+        {status === "error" && <div style={{ color: "red" }} dangerouslySetInnerHTML={{__html: message}}/>}
+        {status === "success" && <div style={{ color: "green" }}>Subscribed !</div>}
+      </div>
+    )}
+  />
+)
+```
+see examples in [./demo/src](./demo/src)
---
+++ demo/src/index.js
@@ -0,0 +1,88 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+import GithubCorner from "react-github-corner";
+
+import LapadiSubscribe from "../../src";
+
+const CustomForm = ({ status, message, onValidated }) => {
+  console.log("pr", process)
+  let email, name;
+  const submit = () =>
+    email &&
+    name &&
+    email.value.indexOf("@") > -1 &&
+    onValidated({
+      email: email.value,
+      name: name.value
+    });
+
+  return (
+    <div
+      style={{
+        borderRadius: 2,
+        padding: 10,
+        justifyContent: "center"
+      }}
+    >
+      {status === "sending" && <div style={{ color: "blue" }}>sending...</div>}
+      {status === "error" && (
+        <div
+          style={{ color: "red" }}
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      {status === "success" && (
+        <div
+          style={{ color: "green" }}
+          dangerouslySetInnerHTML={{ __html: message }}
+        />
+      )}
+      <input
+        style={{  padding: 5 }}
+        ref={node => (name = node)}
+        type="text"
+        placeholder="Your name"
+      />
+      <br />
+      <input
+        style={{  padding: 5 }}
+        ref={node => (email = node)}
+        type="email"
+        placeholder="Your email"
+      />
+      <br />
+      <button style={{  padding: 5 }} onClick={submit}>
+        Submit
+      </button>
+    </div>
+  );
+};
+
+class Demo extends Component {
+  render() {
+    const url = `https://halildogan.lapadi.com/subscribe/post?u=${process.env.client_id}&id=${process.env.form_id}`;
+    return (
+      <div style={{
+        justifyContent: "center"
+      }}>
+        <h1>react-lapadi-subscribe Demo</h1>
+        <GithubCorner href="https://github.com/lapadi/react-lapadi-subscribe" />
+        <h2>Default Form</h2>
+        <LapadiSubscribe url={url} />
+        <h2>Custom Form</h2>
+        <LapadiSubscribe
+          url={url}
+          render={({ subscribe, status, message }) => (
+            <CustomForm
+              status={status}
+              message={message}
+              onValidated={formData => subscribe(formData)}
+            />
+          )}
+        />
+      </div>
+    );
+  }
+}
+
+render(<Demo />, document.querySelector("#demo"));
